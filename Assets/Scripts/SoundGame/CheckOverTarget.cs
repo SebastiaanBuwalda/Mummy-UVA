c02ad33996fb3f739b73097a9acae5c49fdae03c
Niels <Niels97vet@hotmail.com>
2017-01-17 15:02:28 +0100

    SoundGame 1e commit

---
+++ Assets/Scripts/SoundGame/CheckOverTarget.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckOverTarget : MonoBehaviour {
+
+    private Vector2 boxPosition;
+
+    private DragObjects tileScript;
+    private bool mouseUp = false;
+
+    private bool lockIn = false;
+
+    private GameObject tile;
+
+    // Use this for initialization
+    void Start() {
+        boxPosition = this.transform.position;
+    }
+
+    void update()
+    {
+        if (lockIn & mouseUp)
+        {
+            Debug.Log("help");
+            tile.transform.position = boxPosition;
+            tileScript.AboveTarget = true;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        mouseUp = false;
+    }
+
+    void OnMouseUp()
+    {
+        mouseUp = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Tile")
+        {
+            tileScript = other.gameObject.GetComponent<DragObjects>();
+            tile = other.gameObject;
+            lockIn = true;
+            Debug.Log(lockIn);
+        }
+        else {
+            lockIn = false;                
+        }
+    }
+}
---
+++ Assets/Scripts/SoundGame/DragObjects.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+
+public class DragObjects : MonoBehaviour {
+
+    private Vector2 mousePosition2D;
+
+    private Vector2 originalPosition2D;
+
+    private bool snapToMouse = false;
+
+    private bool aboveTarget = false;
+
+    [SerializeField]
+    private int tileNumber;
+
+    public bool AboveTarget;
+
+    void Start()
+    {
+        originalPosition2D = this.transform.position;
+    }
+
+    void OnMouseDown()
+    {
+        snapToMouse = true;
+    }
+
+    void OnMouseUp()
+    {
+        snapToMouse = false;
+
+        aboveTarget = AboveTarget;
+
+        if (aboveTarget)
+        {
+            Debug.Log("target locked");
+        }
+        else {
+            this.transform.position = originalPosition2D;
+        }
+    }
+
+    void Update()
+    {
+        if (snapToMouse)
+        {
+            mousePosition2D = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            this.transform.position = mousePosition2D;
+        }
+    }
+
+    public bool SnapToMouse
+    {
+        get
+        {
+            return snapToMouse;
+        }
+    }
+
+    public int TileNumber
+    {
+        get
+        {
+            return tileNumber;
+        }
+    }
+}
