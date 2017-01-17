43e5c9c31b39974fa8e59cb37a6e6b5728bb4c26
ezowolf <wiggler@live.nl>
2017-01-17 12:53:24 +0100

    Sync for Folders

---
+++ Assets/Scripts/MemoryGame/CardSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSpawner : MonoBehaviour {
+
+	[SerializeField]
+	private List<Vector2> cardPositions = new List<Vector2>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
