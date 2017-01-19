2e4cc955b4d0ffba794b37855033e87c672f8aa6
Phineas Moore <phineasmoore@gmail.com>
2017-01-19 12:55:30 +0100

    YAY?!

---
+++ Assets/Scripts/UI/ScreenShot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour {
+
+	public void CaptureScreen()
+    {
+        Application.CaptureScreenshot("ScreenShotImage.png", 4);
+    }
+		
+}
