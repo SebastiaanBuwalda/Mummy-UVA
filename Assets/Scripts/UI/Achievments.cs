594abbcb1542dac033730f6e7afa1a123b33a5f9
Phineas Moore <phineasmoore@gmail.com>
2017-01-19 16:51:49 +0100

    Push

---
+++ Assets/Scripts/UI/Achievments.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Achievments : MonoBehaviour {
+
+    [SerializeField]
+    private int score;
+
+    [SerializeField]
+    public Color gold, silver, bronze;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CheckScore()
+    {
+        if (score <= 49)
+        {
+            
+        }
+
+        else if (score > 49 && score <=74)
+        {
+
+        }
+
+        else if (score >= 75)
+        {
+
+        }
+    }
+}
