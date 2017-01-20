78b35a7f3d343d810075f2c6792316169f7d2ae1
Niels <Niels97vet@hotmail.com>
2017-01-20 11:44:47 +0100

    SoundGame sound start

---
+++ Assets/Scripts/SoundGame/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip tileSound;
+
+    private AudioSource audio;
+
+    // Use this for initialization
+    void Start () {
+        audio = gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown() {
+        playSound();
+    }
+
+    private void playSound()
+    {
+        audio.Stop();
+        audio.PlayOneShot(tileSound);
+    }
+}
