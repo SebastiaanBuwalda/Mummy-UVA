4583ba37f8043257c041e0375b3152a1a2919651
Phineas Moore <phineasmoore@gmail.com>
2017-01-20 14:02:37 +0100

    UI stuff

--- Assets/Scripts/UI/Achievments.cs
+++ Assets/Scripts/UI/Achievments.cs
@@ -9,7 +9,16 @@ public class Achievments : MonoBehaviour {
     private int score;
 
     [SerializeField]
-    public Color gold, silver, bronze;
+    private Color gold = new Color(255f, 216f, 0f);
+
+    [SerializeField]
+    private Color silver = new Color(238f, 238f, 238f);
+
+    [SerializeField]
+    private Color bronze = new Color(255f, 174f, 0f);
+
+    [SerializeField]
+    private Image medal;
 
 	// Use this for initialization
 	void Start () {
@@ -18,24 +27,27 @@ public class Achievments : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
     void CheckScore()
     {
         if (score <= 49)
         {
-            
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();  //Get the renderer via GetComponent or have it cached previously
+            renderer.color = bronze; // Set to opaque black
         }
 
-        else if (score > 49 && score <=74)
+        else if (score >= 50 && score <=74)
         {
-
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();  //Get the renderer via GetComponent or have it cached previously
+            renderer.color = silver; // Set to opaque black
         }
 
         else if (score >= 75)
         {
-
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();  //Get the renderer via GetComponent or have it cached previously
+            renderer.color = gold; // Set to opaque black
         }
     }
 }
