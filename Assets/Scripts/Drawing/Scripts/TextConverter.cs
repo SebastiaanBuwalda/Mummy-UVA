62ca96782f3e7bcc7dbc992e6da51905503e45f3
JoeyWelvaadt1999 <joeywelvaadt@gmail.com>
2017-01-25 11:19:04 +0100

    Drawing game

---
+++ Assets/Scripts/Drawing/Scripts/TextConverter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextConverter : MonoBehaviour {
+	[SerializeField]private InputField _inputText;
+	[SerializeField]private Text _outputText;
+
+	private void Update() {
+		_outputText.text = _inputText.text;
+	}
+}
