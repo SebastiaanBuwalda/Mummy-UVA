d5d9c61bfaf35d690bf12d6236c2c253c24f9321
Phineas Moore <phineasmoore@gmail.com>
2017-01-25 12:56:58 +0100

    Splash Screen and End Screen Added

---
+++ Assets/Scripts/UI/Quit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour {
+
+    [SerializeField]
+    private int timeToWait = 3;
+
+  
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(QuitGame());
+    }
+
+    IEnumerator QuitGame()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        Application.Quit();
+
+
+    }
+}
