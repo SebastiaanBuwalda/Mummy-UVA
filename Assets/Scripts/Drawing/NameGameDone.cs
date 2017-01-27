8b7dd980b6f827a5f852467a1c21e344e2f1dae2
Niels <Niels97vet@hotmail.com>
2017-01-27 21:21:36 +0100

    NameGameSounds

---
+++ Assets/Scripts/Drawing/NameGameDone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameGameDone : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip completeSound;
+
+    [SerializeField]
+    private AudioSource audio;
+
+    [SerializeField]
+    private float timeToWait;
+
+    private bool pressOnce = true;
+
+    public string levelToLoad = "NameGameToOddOneOutGame";
+
+    public void NewScene()
+    {
+        if (pressOnce)
+        {
+            StartCoroutine(SwitchScene());
+        }
+    }
+
+    IEnumerator SwitchScene()
+    {
+        audio.Stop();
+        audio.PlayOneShot(completeSound);
+        pressOnce = false;
+        yield return new WaitForSeconds(timeToWait);
+        Application.LoadLevel(levelToLoad);
+    }
+
+}
