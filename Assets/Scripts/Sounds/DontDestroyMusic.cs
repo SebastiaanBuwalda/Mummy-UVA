b73d7b0ee5ca9977d36b536b24c7b51fe6d9b3c2
Phineas Moore <phineasmoore@gmail.com>
2017-01-27 16:25:57 +0100

    Background Music Added

---
+++ Assets/Scripts/Sounds/DontDestroyMusic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyMusic : MonoBehaviour {
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("BackgroundMusic");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
