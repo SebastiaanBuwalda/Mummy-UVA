4bb3272350f1ed9dcf0d9826b2e801ed199e9c7f
ezowolf <wiggler@live.nl>
2017-01-27 12:50:48 +0100

    Drawing Game!

---
+++ Assets/Scripts/Drawing/DrawingSaverScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawingSaverScript : MonoBehaviour {
+
+	public static List<GameObject> DrawingList = new List<GameObject>();
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	static void SpawnDrawing()
+	{
+		foreach (GameObject dot in DrawingList) {
+			Instantiate (dot);
+		}
+	}
+}
