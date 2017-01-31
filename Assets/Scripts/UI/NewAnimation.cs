d8bdcc7edbabc08f9ff55ede0797e13335685c11
Phineas Moore <phineasmoore@gmail.com>
2017-01-31 15:53:30 +0100

    Interactive Scene Added! GAME IS FINISHED

---
+++ Assets/Scripts/UI/NewAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewAnimation : MonoBehaviour {
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void OnMouseDown () {
+        if (animator.GetBool("isHit")==false) {
+        animator.SetBool("isHit", true);
+        
+        }
+    }
+
+
+    public void PlaySound(AudioClip audioClip)
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(audioClip);
+        StartCoroutine( SwitchScene());
+    }
+
+    IEnumerator SwitchScene()
+    {
+        yield return new WaitForSeconds(3);
+        Application.LoadLevel("firstScene");
+    }
+}
